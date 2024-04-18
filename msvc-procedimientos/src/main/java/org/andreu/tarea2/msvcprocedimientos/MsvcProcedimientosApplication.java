b7d01880b6e33eb3b236dfbc00d3607b68cb8ea8
Andreu Laguarda <andreulaguarda@gmail.com>
2024-04-18 16:17:17 +0200

    Version Final

--- msvc-procedimientos/src/main/java/org/andreu/tarea2/msvcprocedimientos/MsvcProcedimientosApplication.java
+++ msvc-procedimientos/src/main/java/org/andreu/tarea2/msvcprocedimientos/MsvcProcedimientosApplication.java
@@ -6,15 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
+// Aplicación Spring Boot para el microservicio de Procedimientos
 @SpringBootApplication
 @EnableFeignClients
 public class MsvcProcedimientosApplication {
 
+	// Bean para ModelMapper (mapeo de entidades)
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
 
+	// Método main de la aplicación Spring Boot
 	public static void main(String[] args) {
 		SpringApplication.run(MsvcProcedimientosApplication.class, args);
 	}
